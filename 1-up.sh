e7112ac8d36566c029ee5ef9c4f179ac8eb88338
zardoz-ilbarbaro <davide.romboli@gmail.com>
2025-12-09 11:35:01 +0100

    update

--- 1-up.sh
+++ 1-up.sh
@@ -18,32 +18,24 @@
 #tput setaf 8 = light blue
 ##################################################################################################################
 
-#source="/home/zardoz/ARCH-ISO/AA/2-setup-git.sh"
-
-# change a commit comment
-# git commit --amend -m "more info"
-# git push --force origin
-
-# stash or commit
-# git reset --hard
-
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
 git pull
 
+# reset - commit your changes or stash them before you merge
+# git reset --hard - personal alias - grh
+
+if [[ -f "./repo.sh" ]]; then
+    echo "Found repo.sh, running it..."
+    bash ./repo.sh
+fi
+
 # Below command will backup everything inside the project folder
 git add --all .
 
-# Give a comment to the commit if you want
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
-
-read input
-
 # Committing to the local repository with a message containing and commit text
 
-git commit -m "$input"
+git commit -m "update"
 
 # Push the local files to github
 
