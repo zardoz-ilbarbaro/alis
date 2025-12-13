f9e1071c9b7a978cb2618f0ffcdd4d605ffaffb2
zardoz-ilbarbaro <davide.romboli@gmail.com>
2025-12-13 12:49:41 +0100

    add script

---
+++ files/home/zardoz/0-git-nemesis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo
+echo "################################################################"
+echo "Shall we start the zardoz-nemesis script"
+echo
+echo "Answer with y/Y or n/N"
+echo "################################################################"
+echo
+read response
+if [[ "$response" == [yY] ]]; then
+
+git clone https://github.com/zardoz-ilbarbaro/zardoz-nemesis.git
+
+echo
+echo "################################################################"
+echo "####  DONE                             #########################"
+echo "################################################################"
+echo
